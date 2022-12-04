c48eb8b236c8b5b152a2b619a51bc44c6a070dd8
Vlad Lomakin <v.lomakin1@g.nsu.ru>
2022-12-04 18:24:18 +0700

    Теперь красиво когда на кнопку наводишь

--- src/components/UI/button/MyButton.jsx
+++ src/components/UI/button/MyButton.jsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import classes from './MyButton.module.css';
+import useHover from "../../../hooks/useHover";
 
 const MyButton = ({children, ...props}) => {
-    return (
-        <button {...props} className={classes.myBtn}>
-            {children}
-        </button>
-    );
+    const ref = useRef()
+    const isHover = useHover(ref)
+    if(isHover) {
+        return (
+            <button ref = {ref} {...props} className={classes.myActBtn}>
+                {children}
+            </button>
+        );
+    } else {
+        return (
+            <button ref = {ref} {...props} className={classes.myBtn}>
+                {children}
+            </button>
+        );
+    }
 };
 
 export default MyButton;
