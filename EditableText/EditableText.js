5339661e806779627410d4992fa41c8af43ef837
ploddasha <91487758+ploddasha@users.noreply.github.com>
2023-03-21 13:45:31 +0700

    Add files via upload

---
+++ EditableText/EditableText.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import component6 from "./assets/Component6.png";
+
+function EditableText() {
+    const [isEditable, setIsEditable] = useState(false);
+    const [text, setText] = useState('Your text here');
+
+    const handleEditClick = () => {
+        setIsEditable(true);
+    }
+
+    const handleInputChange = (e) => {
+        setText(e.target.value);
+    }
+
+    const handleSaveClick = () => {
+        setIsEditable(false);
+    }
+
+    return (
+        <div>
+            {isEditable ? (
+                <div>
+                    <div className="wrapper">
+                        <input type="text" value={text} onChange={handleInputChange} />
+
+                    </div>
+
+                    <button className="image-button2" onClick={handleSaveClick}>
+                        <div style={{ backgroundImage: `url(${component6})` }}></div>
+                    </button>
+                </div>
+            ) : (
+                <div>
+                    <div className="wrapper">
+                        <p>{text}</p>
+
+                    </div>
+
+
+                    <button className="image-button2" onClick={handleEditClick}>
+                        <div style={{ backgroundImage: `url(${component6})` }}></div>
+                    </button>
+                </div>
+            )}
+        </div>
+    );
+}
+
+export default EditableText;
+
+//<button onClick={handleSaveClick}>Save</button>
+//<button onClick={handleEditClick}>Edit</button>
+//<p>{text2}</p>
