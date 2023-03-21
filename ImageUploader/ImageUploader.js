206b371d636992c62bb2bb150d8f779cb7cfc1bd
ploddasha <91487758+ploddasha@users.noreply.github.com>
2023-03-21 13:49:28 +0700

    Add files via upload

---
+++ ImageUploader/ImageUploader.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import photo from "./assets/photo.png";
+import { Resizer } from "react-image-file-resizer";
+
+
+function ImageUploader() {
+    const [selectedFile, setSelectedFile] = useState(null);
+
+    const handleFileChange = (event) => {
+        setSelectedFile(URL.createObjectURL(event.target.files[0]));
+
+    };
+    const imageStyle = selectedFile ? { backgroundImage: `url(${selectedFile})` } : { backgroundImage: `url(${photo})` };
+
+    return (
+        <div className="image-2">
+            {selectedFile ? (
+                <img className="thumbnail" src={selectedFile} alt="selected" />
+            ) : (
+                <label>
+                    <div>
+
+                        <input type="file" onChange={handleFileChange} style={{ display: 'none' }}/>
+                            <div style={{ backgroundImage: `url(${photo})` }}></div>
+                    <div className="image-placeholder" style={imageStyle}></div>
+                    </div>
+                </label>
+            )}
+        </div>
+    );
+}
+
+export default ImageUploader;
+
